64b50a42b92c728c0c62e0437e0289a8e80bec2c
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-18 10:17:04 +0700

    up

--- controller/Users.js
+++ controller/Users.js
@@ -1,24 +1,254 @@
+import { Op } from "sequelize";
 import Users from "../models/UsersModel.js";
+import Faculty from "../models/FacultyModel.js";
 
 export const getUser = async (req, res) => {
-  try {
-    const users = await Users.findAll();
+  const { faculty = "all", name = "all", role = "all" } = req.query;
+  const page = parseInt(req.query.page) - 1 || 0;
+  const limit = parseInt(req.query.limit) || 5;
+  const offset = limit * page;
 
-    if (users.length > 0) {
-      return res.status(200).json({
-        status: 200,
-        data: users,
+  try {
+    if (faculty == "all" && name == "all" && role == "all") {
+      const totalRows = await Users.count();
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty == "all" && name == "all" && role != "all") {
+      const totalRows = await Users.count({
+        where: { role: role },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: { role: role },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty == "all" && name != "all" && role == "all") {
+      const totalRows = await Users.count({
+        where: { name: { [Op.substring]: name } },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: { name: { [Op.substring]: name } },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty != "all" && name == "all" && role == "all") {
+      const totalRows = await Users.count({
+        where: { idFaculty: faculty },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: { idFaculty: faculty },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty == "all" && name != "all" && role != "all") {
+      const totalRows = await Users.count({
+        where: {
+          [Op.and]: [{ name: { [Op.substring]: name } }, { role: role }],
+        },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: {
+          [Op.and]: [{ name: { [Op.substring]: name } }, { role: role }],
+        },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty != "all" && name == "all" && role != "all") {
+      const totalRows = await Users.count({
+        where: {
+          [Op.and]: [{ idFaculty: faculty }, { role: role }],
+        },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: {
+          [Op.and]: [{ idFaculty: faculty }, { role: role }],
+        },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
+      });
+    } else if (faculty != "all" && name != "all" && role == "all") {
+      const totalRows = await Users.count({
+        where: {
+          [Op.and]: [
+            { name: { [Op.substring]: name } },
+            { idFaculty: faculty },
+          ],
+        },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: {
+          [Op.and]: [
+            { name: { [Op.substring]: name } },
+            { idFaculty: faculty },
+          ],
+        },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
       });
     } else {
-      return res.status(404).json({
-        status: 404,
-        message: "Data Not Found",
+      const totalRows = await Users.count({
+        where: {
+          [Op.and]: [
+            { idFaculty: faculty },
+            { name: { [Op.substring]: name } },
+            { role: role },
+          ],
+        },
+      });
+
+      const totalPage = Math.ceil(totalRows / limit);
+
+      const user = await Users.findAll({
+        where: {
+          [Op.and]: [
+            { idFaculty: faculty },
+            { name: { [Op.substring]: name } },
+            { role: role },
+          ],
+        },
+        order: [["name", "ASC"]],
+        offset: offset,
+        limit: limit,
+        include: [Faculty],
+      });
+
+      res.status(user.length ? 200 : 404).json({
+        status: user.length ? 200 : 404,
+        message: user.length ? "Data Found" : "Data Not Found",
+        data: user.length ? user : null,
+        page: page + 1,
+        limit: limit,
+        rows: offset + 1,
+        rowsPage: offset + 1 + user.length - 1,
+        totalRows: user.length ? totalRows : null,
+        totalPage: user.length ? totalPage : null,
       });
     }
-  } catch {
+  } catch (error) {
     return res.status(500).json({
       status: 500,
-      message: "Internal Server Error",
+      message: error,
     });
   }
 };
